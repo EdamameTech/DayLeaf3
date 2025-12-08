eb3fb35eeff73e59425b244488e3f291cec02be8
zunda <zundan@gmail.com>
2025-12-08 13:53:50 -1000

    Stop using Scaffold

--- app/src/main/java/com/edamametech/android/dayleaf3/ui/DayLeaf3Screen.kt
+++ app/src/main/java/com/edamametech/android/dayleaf3/ui/DayLeaf3Screen.kt
@@ -1,19 +1,20 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.edamametech.android.dayleaf3.ui.theme.DayLeaf3Theme
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
 import java.util.Locale
 
 fun noteDateString(noteDate: LocalDate) : String {
@@ -24,23 +25,22 @@ fun noteDateString(noteDate: LocalDate) : String {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DayLeaf3Screen(noteDate: LocalDate) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
-                ),
-                title = {
-                    Text(noteDateString(noteDate))
-                }
+    Column{
+        Row(
+            verticalAlignment = Alignment.Top,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primaryContainer)
+        ) {
+            Text(
+                noteDateString(noteDate),
+                color = MaterialTheme.colorScheme.primary,
+                maxLines = 1,
+                overflow = TextOverflow.StartEllipsis,
+                modifier = Modifier
+                    .padding(8.dp)
             )
         }
-    ) { innerPadding ->
-        Text(
-            text = "Hello, World!",
-            modifier = Modifier.padding(innerPadding)
-        )
     }
 }
 
