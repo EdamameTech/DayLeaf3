66451738f4ffb2466a8d88e799d0cb65935223a1
zunda <zundan@gmail.com>
2026-03-07 16:37:11 -1000

    Start using ViewModel

--- app/src/main/java/com/edamametech/android/dayleaf3/MainActivity.kt
+++ app/src/main/java/com/edamametech/android/dayleaf3/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawingPadding
@@ -13,37 +14,46 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.edamametech.android.dayleaf3.ui.theme.DayLeaf3Theme
+import com.edamametech.android.dayleaf3.ui.NoteViewModel
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 
 class MainActivity : ComponentActivity() {
-    override fun onPause() {
-        super.onPause()
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val noteDate = LocalDate.now()
 
-        setContent {
-            DayLeaf3Theme {
-                enableEdgeToEdge(
-                    statusBarStyle = if (!isSystemInDarkTheme()) {
-                        SystemBarStyle.light(
-                            MaterialTheme.colorScheme.primaryContainer.toArgb(),
-                            MaterialTheme.colorScheme.primaryContainer.toArgb()
-                        )
-                    } else {
-                        SystemBarStyle.dark(
-                            MaterialTheme.colorScheme.primaryContainer.toArgb()
-                        )
+        val viewModel: NoteViewModel by viewModels()
+        viewModel.setDate(noteDate)
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect {
+                    setContent {
+                        DayLeaf3Theme {
+                            enableEdgeToEdge(
+                                statusBarStyle = if (!isSystemInDarkTheme()) {
+                                    SystemBarStyle.light(
+                                        MaterialTheme.colorScheme.primaryContainer.toArgb(),
+                                        MaterialTheme.colorScheme.primaryContainer.toArgb()
+                                    )
+                                } else {
+                                    SystemBarStyle.dark(
+                                        MaterialTheme.colorScheme.primaryContainer.toArgb()
+                                    )
+                                }
+                            )
+                            Surface(
+                                modifier = Modifier.safeDrawingPadding().fillMaxSize(),
+                            ) {
+                                DayLeaf3Screen(it.date, it.note)
+                            }
+                        }
                     }
-                )
-
-                Surface(
-                    modifier = Modifier.safeDrawingPadding().fillMaxSize(),
-                ) {
-                    DayLeaf3Screen(noteDate, "")
                 }
             }
         }
