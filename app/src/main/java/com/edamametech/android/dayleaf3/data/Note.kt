adb19940bd5d2a43f84fdcc7e95a87f3c48498ab
zunda <zundan@gmail.com>
2026-03-08 16:08:11 -1000

    Add type converters

--- app/src/main/java/com/edamametech/android/dayleaf3/data/Note.kt
+++ app/src/main/java/com/edamametech/android/dayleaf3/data/Note.kt
@@ -3,7 +3,17 @@ package com.edamametech.android.dayleaf3.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import java.time.LocalDate
+
+class Converters {
+    @TypeConverter
+    fun fromLocalDate(localDate: LocalDate): String? = localDate?.toString()
+
+    @TypeConverter
+    fun localDateFromString(string: String?): LocalDate? = LocalDate.parse(string)
+}
 
 @Entity(tableName = "notes")
 data class Note(
