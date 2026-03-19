25736934510aef1f2f20b4dfaec32c034169f460
zunda <zundan@gmail.com>
2026-03-18 16:24:48 -1000

    Implement menu buttons
    Along wtih mock data

--- app/src/main/java/com/edamametech/android/dayleaf3/ui/DayLeaf3Screen.kt
+++ app/src/main/java/com/edamametech/android/dayleaf3/ui/DayLeaf3Screen.kt
@@ -4,8 +4,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,6 +18,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,10 +35,38 @@ fun noteDateString(noteDate: LocalDate): String {
 
 @Composable
 fun DayLeaf3Screen() {
+    /* Mock notes to be replaced by database */
+    var allNotes = hashMapOf<LocalDate, String>(
+        LocalDate.of(2026, 3, 14) to "It is pie day!",
+        LocalDate.of(2026, 3, 17) to "It is Saint Pattick's Day!",
+        LocalDate.now() to "Hello, World! It is today!"
+    )
+    var allDates = allNotes.keys.sorted()
+
+    /* UI state */
     var noteDate by rememberSaveable { mutableStateOf(LocalDate.now()) }
-    var noteText by rememberSaveable { mutableStateOf("Hello, World! from Screen.") }
+    var noteText by rememberSaveable { mutableStateOf(allNotes[noteDate] ?: "") }
+    var isToday = noteDate.isEqual(LocalDate.now())
+    var isFirstDate = noteDate.isEqual(allDates[0])
+
     fun updateNote(text: String) {
         noteText = text
+    }
+
+    fun setDate(date: LocalDate) {
+        noteDate = date
+        noteText = allNotes[date] ?: ""
+    }
+
+    fun offsetDate(offset: Int) {
+        var t = allDates.binarySearch(noteDate) + offset
+        if (t < 0) {
+            t = 0
+        }
+        if (t >= allDates.size) {
+            t = allDates.size - 1
+        }
+        setDate(allDates[t])
     }
 
     Column {
@@ -50,7 +82,44 @@ fun DayLeaf3Screen() {
                 maxLines = 1,
                 overflow = TextOverflow.StartEllipsis,
                 modifier = Modifier
+                    .weight(1F)
                     .padding(8.dp)
+            )
+            /* Export */
+            OutlinedButton(
+                enabled = false,
+                onClick = { /* TODO */ },
+                content = {
+                    Text("^")
+                },
+                shape = RoundedCornerShape(4.dp)
+            )
+            /* Previous day */
+            OutlinedButton(
+                enabled = !isFirstDate,
+                onClick = { offsetDate(-1) },
+                content = {
+                    Text("<")
+                },
+                shape = RoundedCornerShape(4.dp)
+            )
+            /* Next day */
+            OutlinedButton(
+                enabled = !isToday,
+                onClick = { offsetDate(1) },
+                content = {
+                    Text(">")
+                },
+                shape = RoundedCornerShape(4.dp)
+            )
+            // Today
+            OutlinedButton(
+                enabled = !isToday,
+                onClick = { setDate(LocalDate.now()) },
+                content = {
+                    Text(">>")
+                },
+                shape = RoundedCornerShape(4.dp)
             )
         }
         BasicTextField(
