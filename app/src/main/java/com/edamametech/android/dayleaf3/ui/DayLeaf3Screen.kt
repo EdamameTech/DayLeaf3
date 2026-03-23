4775b33041ca34e4622d5cd4a8437cf5007229ab
zunda <zundan@gmail.com>
2026-03-22 17:03:09 -1000

    Make utility function private

--- app/src/main/java/com/edamametech/android/dayleaf3/ui/DayLeaf3Screen.kt
+++ app/src/main/java/com/edamametech/android/dayleaf3/ui/DayLeaf3Screen.kt
@@ -25,7 +25,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
-fun noteDateString(noteDate: LocalDate): String {
+private fun noteDateString(noteDate: LocalDate): String {
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd E", Locale.US)
     return noteDate.format(formatter)
 }
